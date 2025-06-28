c3d483bc5f1b9149b283531acaf1521f918f3c2d
rijalsanjit@gmail.com <rijalsanjit@gmail.com>
2025-06-28 23:02:07 +1000

    home page completed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Product from "./Pages/Product";
 import ShopCategory from "./Pages/ShopCategory";
 import Cart from "./Pages/Cart";
 import LoginSignup from "./Pages/LoginSignup";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginSignup />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -47,8 +47,10 @@ const Navbar = () => {
         </li>
       </ul>
       <div className="nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt="" />
+        
+          <Link to="login"><button>Login</button></Link>
+        
+      <Link to='cart'> <img src={cart_icon} alt="" /></Link> 
         <div className="nav-cart-count">0</div>
       </div>
     </div>
